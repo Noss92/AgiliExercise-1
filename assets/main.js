ff7ad29149918d6d4e06a097d160b8cea973d1b0
Jensone <jeancharlesjensone@gmail.com>
2023-03-08 22:40:50 +0100

    Préparation de tout les éléments pour l'exercice

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+/**
+ * Rédige ton code JavaScript en dessous
+ * et n'oublie pas de lier ta feuille JavaScript avec ta feuille HTML
+**/
+
+// Commence par déclarer les variabls necessaires
+
+
+
+// Rédige les conditions ou fonctions juste ici, n'oublie pas le résultat attendu dans le README.md
+
+
