70878c5e9b4f522f33c376f7b24e1609dbfdcb2e
julien <noss928@gmail.com>
2023-03-09 12:33:55 +0100

    ajout de class active

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,28 @@
 // Rédige les conditions ou fonctions juste ici, n'oublie pas le résultat attendu dans le README.md
 
 
+// const cardButton = document.querySelector('.openCard')
+// const cardHeader = document.querySelector('.card')
+// const cardContent = document.querySelector('.card_content')
+// const cardList = document.querySelector('.card_list')
+// const cardFooter = document.querySelector('.card_footer')
+
+// cardButton.addEventListener("click", openCard)
+
+// function openCard() {
+//     cardHeader.classList.toggle('active')
+//     cardContent.classList.toggle('active')
+//     cardList.classList.toggle('active')
+//     cardFooter.classList.toggle('active')
+// }
+
+
+const sidebarMenu = document.querySelector('.sidebar_menu')
+const sideBar = document.querySelector('.sidebar')
+
+
+sidebarMenu.addEventListener("click",sidebar_menu)
+
+function sidebar_menu(){
+    sideBar.classList.toggle('active')
+}
